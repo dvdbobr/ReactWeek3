4e8ab2f8eee24c7ede15bb5696976fed08e5fe69
David Bobritsky <dvdbobr@gmail.com>
2021-03-30 12:01:49 +0300

    ex19.2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 //import Todo from './w3day1/Components/18.2/Todo';
 //import TimeConverter from "./w3day1/Components/18.3/TimeConverter";
 //import CheckBox from "./w3day1/Components/18.4/CheckBox";
-import Fetch from "./w3day2/Components/Fetch";
+//import Fetch from "./w3day2/Components/Fetch";
+import Countries from "./w3day2/Components/ex19.2/Countries";
 
 function App() {
   return (
     <div>
-      <Fetch/>
+      <Countries/>
     </div>
   );
 }
---
+++ src/w3day2/Components/ex19.2/Countries.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react/cjs/react.development'
+
+export default function Countries() {
+    const [term, setTerm] = useState('')
+    const [countries, setCountries] = useState([])
+    const [filteredCountries, setFilteredCountries] = useState([])
+    useEffect(() => {
+        const search = async () => {
+            const result = await axios.get('https://restcountries.eu/rest/v2/all',)
+            console.log(result.data);
+            setCountries(result.data)
+        }
+        search()
+    }, [])
+    useEffect(() => {
+        const filterCountries = () => {
+            setFilteredCountries(
+                countries.filter(country => { return country.name.toLowerCase().includes(term.toLowerCase()) })
+            )
+        }
+        filterCountries()
+    }, [term,countries])
+
+    return (
+        <div>
+            <input type="text" name="search" onChange={e => setTerm(e.target.value)} />
+            {
+                <ul>
+                    {filteredCountries.map((c,index) => {
+                        return <li key={index}>{c.name}</li>
+                    })}
+                </ul>
+            }
+        </div>
+    )
+}
